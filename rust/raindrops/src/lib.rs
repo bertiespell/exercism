de698a1cbbb8fd6df98cdea8a2a246a1cce7250e
Bertie <berniesnell@gmail.com>
2019-05-29 11:42:41 -0400

    refactor raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,13 +1,13 @@
 pub fn raindrops(n: u32) -> String {
     let mut result = String::from("");
     if n % 3 == 0 {
-        result = result + &String::from("Pling");
+        result += &String::from("Pling");
     }
     if n % 5 == 0 {
-        result = result + &String::from("Plang");
+        result += &String::from("Plang");
     }
     if n % 7 == 0 {
-        result = result + &String::from("Plong");
+        result += &String::from("Plong");
     }
     if result.len() != 0 {
         return result;
