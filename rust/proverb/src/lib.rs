f920a7d705f434d3377e000542b516f373906b8c
Bertie <berniesnell@gmail.com>
2019-05-30 15:30:35 -0400

    add some more notes

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -9,3 +9,25 @@ pub fn build_proverb(list: &[&str]) -> String {
     }
     proverb
 }
+
+use std::iter::once;
+
+// using windows - gives a kind of bundled up view on the data
+
+pub fn build_proverb_refactor(list: &[&str]) -> String {
+    let last = format!("And all for the want of a {}.", &list[0]);
+
+    list
+        .windows(2)
+        .map(|w|build_verse(w[0], w[1]))
+        .chain(once(last)) // chain takes two iterators and creates a new one with both in sequence
+        // here we create a new iterator with once (which makes an iterator that does one this exactly once)
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+
+fn build_verse(left: &str, right: &str) -> String {
+    format!("For want of a {} the {} was lost.", left, right)
+}
+
+// another way to iter over index => for index in 0..(list.len() - 1)
