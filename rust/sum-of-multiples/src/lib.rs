8ec5ae5cf2df9c8675860e16e37e33abd8195b11
Bertie <berniesnell@gmail.com>
2019-05-30 16:29:09 -0400

    small refactor

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -2,10 +2,8 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     (0..limit).fold(0, |acc, x| {
         let mut is_multiple = false;
         for number in factors.into_iter() {
-            if *number != 0 {
-                if x % number == 0 {
-                    is_multiple = true;
-                }
+            if *number != 0 && x % number == 0 {
+                is_multiple = true;
             }
         }
         if is_multiple {
